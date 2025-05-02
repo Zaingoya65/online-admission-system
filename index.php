e7459336a6c2113690e0c0fffe4f80520c1919bd
Zain Ul Abideen <zain65.apply@gmail.com>
2025-05-02 10:50:21 +0000

    meta tags

--- index.php
+++ index.php
@@ -103,8 +103,8 @@ if (isset($_GET['forgot'])) {
 <!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Login - Alhijrah AHRSC</title>
     <script>
--- register.php
+++ register.php
@@ -128,8 +128,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <!doctype html>
 <html>
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Register - Alhijrah AHRSC</title>
     <script>
